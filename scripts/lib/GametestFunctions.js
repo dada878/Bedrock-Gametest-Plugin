dca04386818f1204a26326ff8f2c5847f7198016
= <xx0932399@gmail.com>
2022-04-23 22:18:06 +0800

    重新整理函式庫

--- scripts/lib/GametestFunctions.js
+++ scripts/lib/GametestFunctions.js
@@ -12,44 +12,9 @@ export function logfor(player,message) {
     Minecraft.world.getDimension("overworld").runCommand(`tellraw "${player}" {"rawtext":[{"text":"${okay_message}"}]}`)
 };
 
-export function GetWorldPlayersName() {
-    let callback = cmd("list")
-
-    callback = callback.substring(callback.indexOf('\n')+1,callback.length)
-
-    let result = callback.match(/\S+/)
-
-    for (let i = 0 ; i < result ; i++) {
-        let name = result[i]
-        let detectLocation = name.length-1
-
-        if (name[detectLocation] == ',') {
-            result[i][detectLocation] = ''
-        }
-        
-    }
-
-    return result
-}
-
 export function log(message) {
     let okay_message = message.toString().replaceAll('\"',"''").replaceAll('\\',"/")
     Minecraft.world.getDimension("overworld").runCommand(`tellraw @a {"rawtext":[{"text":"${okay_message}"}]}`).statusMessage
-}
-
-export function GetPlayerTags(Playername) {
-    try {
-        let GetTagsMsg = Minecraft.Commands.run(`tag "${Playername}" list`,Minecraft.World.getDimension("overworld")).statusMessage;
-        let GetTagsListReg = /(:|：)(.*, ).*/
-        let GfReturn = GetTagsMsg.match(GetTagsListReg)[0]
-        if (GfReturn.includes("：")) {GfReturn = GfReturn.substring(GfReturn.indexOf("：")+1,GfReturn.length).split(", ")}
-        else {GfReturn = GfReturn.substring(GfReturn.indexOf(":")+1,GfReturn.length).split(", ")}
-        for (let tag in GfReturn) {GfReturn[tag] = GfReturn[tag].substring(2,GfReturn[tag].length-2)}
-        return GfReturn
-    }
-    catch (error) {
-        return []
-    }
 }
 
 export function GetScores (target, scoreboard) {
@@ -68,21 +33,6 @@ export function SetScores (target, scoreboard, scores) {
     return cmd(`scoreboard players set "${target}" "${scoreboard}" ${scores}`);
 }
 
-export function GetPlayerScoreboards (Playername) {
-    try {
-        let GetScoresMsg = Minecraft.Commands.run(`scoreboard players list "${Playername}"`,Minecraft.World.getDimension("overworld")).statusMessage;
-        let GetScoresListReg = /- \S+(:|：) (\S+) \((\S+)\)/gi
-        let GetScoresValueAndKeyReg = /- \S+(:|：) (\S+) \((\S+)\)/
-        let GetScoresTest = GetScoresMsg.match(GetScoresListReg)
-        let GetScoresDict = {}
-        for (let score in GetScoresTest) {GetScoresDict[GetScoresTest[score].match(GetScoresValueAndKeyReg)[3]] = parseInt(GetScoresTest[score].match(GetScoresValueAndKeyReg)[2],10)}
-        let GfReturn = GetScoresDict
-        return GfReturn
-    }
-    catch (error) {
-        return error
-    }
-}
 /*
 GetPlayerScoreboards(playername) 回傳玩家所有記分板的字典
 GetPlayerTags(playername) 回傳玩家所有標籤的陣列

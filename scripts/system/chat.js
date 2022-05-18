4703c75dc07bb6f6004ba1f7b6011c80603a493f
Mantou <74487898+Mantou1233@users.noreply.github.com>
2022-05-18 20:47:52 +0800

    noone

--- scripts/system/chat.js
+++ scripts/system/chat.js
@@ -7,7 +7,7 @@ import { chatFormat, enables } from "../config.js";
 /**
  * 對玩家顯示稱號選單
  * @param {Minecraft.Player} player 玩家
- * @return none
+ * @return void
  */
 export function ChangeChat(player) {
 

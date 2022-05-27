c0a924f5693f6c4327621587df7ad4b442610a8c
Mantou <74487898+Mantou1233@users.noreply.github.com>
2022-05-27 15:05:43 +0800

    style: semicolon fix

--- scripts/system/shop.js
+++ scripts/system/shop.js
@@ -52,26 +52,26 @@ export function ShopSystem(player) {
             case (0): {
                 let fm = new ui.ActionFormData();
                 fm.title("購買");
-                buyableItems.forEach((f) => {fm.button(f.display, f.icon ?? "")})
+                buyableItems.forEach((f) => {fm.button(f.display, f.icon ?? "")});
 
                 fm.show(player).then(response => {
                     if (!response || response.isCanceled) return;
 
                     if(buyableItems[response.selection]){
-                        const item = buyableItems[response.selection]
-                        let money = moneyTable.getScore(player)
-                        const maxCount = money / item.price
-                        if(moneyTable.getScore(player) < item.price) return logfor(player, `>> 你沒有足夠的金錢買一個${item.display}!`)
+                        const item = buyableItems[response.selection];
+                        let money = moneyTable.getScore(player);
+                        const maxCount = money / item.price;
+                        if(moneyTable.getScore(player) < item.price) return logfor(player, `>> 你沒有足夠的金錢買一個${item.display}!`);
                         let fm = new ui.ModalFormData();
                         fm.slider("你要買多少個？", 0, Math.min(maxCount, maxSelect), 1, maxCount);
 
                         fm.show(player).then((response) => {
-                            let count = response.formValues[0] 
+                            let count = response.formValues[0];
                             cmds([
                                 `give ${player.name} ${item.id} ${response.formValues[0]} `
-                            ])
-                            logfor(player, `>> 成功購買${count}個${item.display}!`)
-                            moneyTable.removeScore(player, count * item.price)
+                            ]);
+                            logfor(player, `>> 成功購買${count}個${item.display}!`);
+                            moneyTable.removeScore(player, count * item.price);
                         })
                     }
                 })
@@ -79,23 +79,23 @@ export function ShopSystem(player) {
             case (1): {
                 let fm = new ui.ActionFormData();
                 fm.title("出售");
-                sellableItems.forEach((f) => {fm.button(f.display, f.icon ?? "")})
+                sellableItems.forEach((f) => {fm.button(f.display, f.icon ?? "")});
                 fm.show(player).then(response => {
                     if (!response || response.isCanceled) return;
 
                     if(sellableItems[response.selection]){
-                        const item = sellableItems[response.selection]
+                        const item = sellableItems[response.selection];
                         const count = getItemCount(item.id, item.data ?? 0, player)[0]?.count || 0;
-                        if(count) return logfor(player, `>> 你沒有足夠的金錢買一個${item.display}!`)
+                        if(count) return logfor(player, `>> 你沒有足夠的物品出售${item.display}!`);
                         let fm = new ui.ModalFormData();
                         fm.slider("你要出售多少個？", 0, Math.min(count, maxSelect), 1, count);
                         fm.show(player).then((response) => {
-                            let count = response.formValues[0] 
+                            let count = response.formValues[0];
                             cmds([
                                 `clear ${player.name} ${item.id} ${response.formValues[0]}`
-                            ])
-                            logfor(player, `>> 成功出售${count}個${item.display}!`)
-                            moneyTable.addScore(player, count * item.price)
+                            ]);
+                            logfor(player, `>> 成功出售${count}個${item.display}!`);
+                            moneyTable.addScore(player, count * item.price);
                         })
                     }
                 })
